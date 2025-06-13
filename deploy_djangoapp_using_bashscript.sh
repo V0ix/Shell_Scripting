80d86729ea38c788c1217c93e46806e91af57acd
V0ix <142316886+V0ix@users.noreply.github.com>
2025-06-13 19:59:05 +0530

    Update deploy_djangoapp_using_bashscript.sh

--- deploy_djangoapp_using_bashscript.sh
+++ deploy_djangoapp_using_bashscript.sh
@@ -1,1 +1,57 @@
+#!/bin/bash
 
+<<comment
+This is minimal version of shell script which is used to deploy django app which is clone from the link: https://github.com/LondheShubham153/django-notes-app.git 
+comment
+
+cloning(){
+  echo "cloning........"
+   if [ -d "django-notes-app" ]; then
+        echo "The code directory already exists. Skipping clone."
+    else
+        git clone https://github.com/LondheShubham153/django-notes-app.git || {
+            echo "Failed to clone the code."
+            return 1
+        }
+    fi
+}
+installment(){
+sudo apt update
+suod apt  install nginx
+sudo apt install -y python3.9
+sudo apt install -y nodejs
+}
+restarting(){
+   echo "Installing dependencies..."
+    sudo apt-get update && sudo apt-get install -y docker.io nginx docker-compose || {
+        echo "Failed to install dependencies."
+        return 1
+    }
+}
+build(){
+echo "Building and deploying the Django app..."
+    docker build -t notes-app . && docker-compose up -d || {
+        echo "Failed to build and deploy the app."
+        return 1
+    }
+}
+
+echo "************** DEPLOYMENT STARTED *************"
+
+if ! cloning;then
+  echo "error occur while cloning"
+  exit 1
+fi
+if ! installment;then
+  echo "installment not done "
+  exit 1
+fi
+if ! restarting;then
+  echo "error occur"
+  exit 1
+fi
+if ! build;then
+  echo "building problem"
+  exit 1
+fi
+echo "************** DEPLOYMENT ENDED *************"
